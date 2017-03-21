7a9cf250db83428731926970f7dbdb945d5dcef6
DESKTOP-CCA9KLI\Nick Kroeger <nkroeger@ufl.edu>
2017-03-21 13:14:50 -0400

    Deleted comment.

--- src/main/java/RandomTileIndexGenerator.java
+++ src/main/java/RandomTileIndexGenerator.java
@@ -40,8 +40,7 @@ public class RandomTileIndexGenerator {
 
         Collection<Integer> tilesPicked = new HashSet<>();
 
-        //Just testing...
-        //test this file can commit
+        //Just testing...Hi
         for(int i = 0; i < 48; i++) {
             System.out.print(i + ". ");
             r.getRandomTileIndex(tilesPicked);
